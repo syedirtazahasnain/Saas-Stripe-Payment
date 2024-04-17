ef906d60dc1d30504b2f43e88c04528b7b7c284c
arsal <arsal.h@yahoo.com>
2024-04-18 02:35:19 +0500

    database structure , seeders , models

---
+++ app/Models/UsedFeature.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UsedFeature extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'credits',
+        'user_id',
+        'feature_id',
+        'data',
+    ];
+
+    /** Casts for jsonb is used data tpo array or array to json */
+    protected function casts(): array
+    {
+        return [
+            'data' => 'array',
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function feature()
+    {
+        return $this->belongsTo(Feature::class);
+    }
+}
